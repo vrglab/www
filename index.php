0d3f706b73cbb2befc52f321c24ddab7ebff1399
vrglab <vrglab1@gmail.com>
2022-03-30 18:43:06 +0200

    idk

--- index.php
+++ index.php
@@ -1,45 +1,255 @@
+
+
+
+
+
 <!DOCTYPE html> 
 
 <meta name="viewport" content="width=device-width, initial-scale=0.5">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Calander</title>
+
+
+
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "callander";
+$something = php_uname();
+
+
+if($_COOKIE["useData"] == null)
+{
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+  $sql = "INSERT INTO users (userDeviceName)
+  VALUES ('$something')";
+  
+  if ($conn->query($sql) === TRUE) {
+      
+
+
+
+
+
+    $sql = "SELECT id, userDeviceName FROM users";
+        $result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+
+    if($row["userDeviceName"] == $something)
+    {
+        setcookie("useData", $row["id"], time() + 2  * 24 * 60 * 60);
+    }
+  }
+}
+
+
+
+
+
+
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+}
+
+
+
+
+
+?>
+
+
 <html lang="de">
 
  <link rel="stylesheet"  href="style.css"> 
+
+
 
 
 <body id="body" class="Norm">
 
 
 
+
+
+
+
 <section >   
         <div class="info" id = "Month">    <?php
-          
-          
-          
-          
 
 
-                $list = ["","","Mär"];
+  
+
+
+   
+          
+        if($_COOKIE["ViewMonthData"] == null)
+        {
+            setcookie("ViewMonthData", date("m"), time() + 2  * 24 * 60 * 60);
+
+          
+        }
+        else
+        {
+         
+        }
+          
+        
+
+        
+                $list = ["Jan","Feb","Mär","Apr","Mai","Jun","Jul","Aug","Sep","Okt","Nov","Dez"];
                 $strDate =  explode(" ", date("year"), 8);
-           
-                $monthNummber = date("m");
 
+                if($_COOKIE["ViewYearData"] == null)
+                {
+                    setcookie("ViewYearData", $strDate[3], time() + 2  * 24 * 60 * 60);
+                }
+
+                if($_COOKIE["CurentDate"] == null)
+                {
+                    setcookie("CurentDate", date("m") . "-" . date("d") . "-" .  $strDate[3], time() + 2  * 24 * 60 * 60);
+        
+                  
+                }
+           
+                $monthNummber = $_COOKIE["ViewMonthData"];
+                $yearNummber =  $_COOKIE["ViewYearData"];
+            
                 switch ($monthNummber) {
                     case '01':
-                       
+                        echo($list[0] . " " . date("d") . " " .  $yearNummber);
                         break;
 
                     case '02':
-                       
+                        echo($list[1] . " " . date("d") . " " .  $yearNummber);
                         break;  
-
+                        
                     case '03':
-                         echo($list[2] . " " . date("d") . " " .  $strDate[3]);
-                        break;                 
+                         echo($list[2] . " " . date("d") . " " .  $yearNummber);
+                        break;
+                    case '04':
+                            echo($list[3] . " " . date("d") . " " .  $yearNummber);
+                           break; 
+                    case '05':
+                            echo($list[4] . " " . date("d") . " " .  $yearNummber);
+                           break;
+                    case '06':
+                            echo($list[5] . " " . date("d") . " " .  $yearNummber);
+                           break;
+                    case '07':
+                            echo($list[6] . " " . date("d") . " " .  $yearNummber);
+                           break;
+                    case '08':
+                            echo($list[7] . " " . date("d") . " " .  $yearNummber);
+                           break;
+                    case '09':
+                            echo($list[8] . " " . date("d") . " " .  $yearNummber);
+                           break; 
+                    case '10':
+                            echo($list[9] . " " . date("d") . " " .  $yearNummber);
+                           break; 
+                    case '11':
+                            echo($list[10] . " " . date("d") . " " .  $yearNummber);
+                           break; 
+                    case '12':
+                            echo($list[11] . " " . date("d") . " " .  $yearNummber);
+                           break;                          
 
 
                 }
+              
+                      
+
+                function NextMonth()
+                {
+                    $cure = $_COOKIE["ViewMonthData"];
+                    $yearNummber =  $_COOKIE["ViewYearData"];
+                    if($cure == 12)
+                    {
+                        $cure = 0;
+                        setcookie("ViewYearData", $yearNummber + 01, time() + 2  * 24 * 60 * 60);
+                    }
+                   setcookie("ViewMonthData", $cure + 01, time() + 2  * 24 * 60 * 60);
+                }
+
+                function PrevMonth()
+                {
+                    $cure = $_COOKIE["ViewMonthData"];
+                    $yearNummber =  $_COOKIE["ViewYearData"];
+                    if($cure == 1)
+                    {
+                        $cure = 13;
+                        setcookie("ViewYearData", $yearNummber - 01, time() + 2  * 24 * 60 * 60);
+                    }
+                   setcookie("ViewMonthData", $cure - 01, time() + 2  * 24 * 60 * 60);
+                }
+
+
+
+                function AddCalData()
+                {
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "callander";
+                    $id = $_COOKIE["useData"];
+                    $da =   '
+                    {
+                        "date": "30-03-2022",
+                        "time": "18:40",
+                        "data": "Hello this is my apointment"
+                      }
+                    ';
+
+
+
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+
+                    if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                    }
+
+                    $sql = "INSERT INTO callanderdata (user_ida, dataa)
+                    VALUES ('$id', ' $da')";
+
+                    if ($conn->query($sql) === TRUE) {
+                  
+                    } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+
+                    $conn->close();
+
+                }
+
+
+                if(array_key_exists('NextMonth', $_POST)) {
+                    NextMonth();
+                    header('Refresh: 1; url=index.php');
+                }
+                else if(array_key_exists('LastMonth', $_POST)) {
+                    PrevMonth();
+                    header('Refresh: 1; url=index.php');
+                }else if(array_key_exists('AddToAppointments', $_POST)) {
+                    AddCalData();
+                 
+                }
+                
+
+
 
           ?>    </div>    
             <div class="clock" id="Clock">
@@ -100,7 +310,7 @@
           
           $strDate =  explode(" ", date("year"), 8);
            
-          $monthNummber = date("m");
+          $monthNummber = $_COOKIE["ViewMonthData"];
          $d=cal_days_in_month(CAL_GREGORIAN,$monthNummber,$strDate[3]);
 
 
@@ -143,7 +353,7 @@
                 
                 
                        
-                                $element->setAttribute('class', 'cell');
+                                $element->setAttribute('class', 'cellCurent');
                                 $dom->appendChild($element);
                 
                                 echo $dom->saveHTML();
@@ -270,47 +480,53 @@
                         }
       
                     }else{
-
-                        if($dayOfWeek == "Monday")
+                        if($da == date("d"))
                         {
-                            $element = $dom->createElement('div',
-                            $da);
-                           
-              
-                         $attr = $element->setAttributeNode(
-                                   new DOMAttr('class', 'cell'));
-            
-            
-            
-                   
-                            $element->setAttribute('class', 'cell');
-                            $dom->appendChild($element);
-            
-                            echo $dom->saveHTML();
+                            if($dayOfWeek == "Monday")
+                            {
+                                $element = $dom->createElement('div',
+                                $da);
+                               
+                  
+                             $attr = $element->setAttributeNode(
+                                       new DOMAttr('class', 'cell'));
+                
+                
+                
+                       
+                                $element->setAttribute('class', 'cellCurent');
+                                $dom->appendChild($element);
+                
+                                echo $dom->saveHTML();
+                            }
+                        }else{
+                            if($dayOfWeek == "Monday")
+                            {
+                                $element = $dom->createElement('div',
+                                $da);
+                               
+                  
+                             $attr = $element->setAttributeNode(
+                                       new DOMAttr('class', 'cell'));
+                
+                
+                
+                       
+                                $element->setAttribute('class', 'cell');
+                                $dom->appendChild($element);
+                
+                                echo $dom->saveHTML();
+                            }
                         }
-    
+
+
 
 
                  
                     }
 
-               
-
-
-
-                  
-
-
-                 
- 
             
                 }
-
-
-        
-            
-
-          
 
           
           ?> </div>
@@ -319,7 +535,7 @@
           
           $strDate =  explode(" ", date("year"), 8);
            
-          $monthNummber = date("m");
+          $monthNummber = $_COOKIE["ViewMonthData"];
           $d=cal_days_in_month(CAL_GREGORIAN,$monthNummber,$strDate[3]);
 
 
@@ -367,7 +583,7 @@
                 
                 
                        
-                                $element->setAttribute('class', 'cell');
+                                $element->setAttribute('class', 'cellCurent');
                                 $dom->appendChild($element);
                 
                                 echo $dom->saveHTML();
@@ -389,6 +605,7 @@
                     
                                     echo $dom->saveHTML();
                                 }
+
 
                       
                                 break;
@@ -526,23 +743,9 @@
                  
                     }
 
-               
 
-
-
-                  
-
-
-                 
- 
             
                 }
-
-
-        
-            
-
-          
 
           
           ?> 
@@ -552,7 +755,7 @@
           
           $strDate =  explode(" ", date("year"), 8);
            
-          $monthNummber = date("m");
+          $monthNummber = $_COOKIE["ViewMonthData"];
           $d=cal_days_in_month(CAL_GREGORIAN,$monthNummber,$strDate[3]);
 
 
@@ -604,7 +807,7 @@
                 
                 
                        
-                                $element->setAttribute('class', 'cell');
+                                $element->setAttribute('class', 'cellCurent');
                                 $dom->appendChild($element);
                 
                                 echo $dom->saveHTML();
@@ -746,28 +949,11 @@
                  
                     }
 
-               
-
-
-
-                  
-
-
-                 
- 
-            
                 }
-
-
-        
-            
-
-          
 
           
           ?> 
         
-
 
         </div>
         <div class="header" id="Thursday">Donnerstag
@@ -775,8 +961,8 @@
           
           $strDate =  explode(" ", date("year"), 8);
            
-          $monthNummber = date("m");
-          $d=cal_days_in_month(CAL_GREGORIAN,date("m"),$strDate[3]);
+          $monthNummber = $_COOKIE["ViewMonthData"];
+          $d=cal_days_in_month(CAL_GREGORIAN,$monthNummber,$strDate[3]);
 
 
 
@@ -784,7 +970,7 @@
                 for ($i=0; $i < $d; $i++) { 
                     $da = $i + 1;
 
-                    $date = $strDate[3] . "-" . date("m") . "-" .  $da;
+                    $date = $strDate[3] . "-" . $monthNummber . "-" .  $da;
 
        
                     $unixTimestamp = strtotime($date);
@@ -956,31 +1142,12 @@
                  
                     }
 
-               
-
-
-
-                  
-
-
-                 
- 
             
                 }
-
-
-        
-            
-
-          
 
           
           ?> 
         
-
-
-        
-
 
         </div>
         <div class="header" id="Friday">Freitag
@@ -988,7 +1155,7 @@
           
           $strDate =  explode(" ", date("year"), 8);
            
-          $monthNummber = date("m");
+          $monthNummber = $_COOKIE["ViewMonthData"];
           $d=cal_days_in_month(CAL_GREGORIAN,$monthNummber,$strDate[3]);
 
 
@@ -1156,31 +1323,12 @@
                  
                     }
 
-               
-
-
-
-                  
-
-
-                 
- 
-            
                 }
 
 
-        
-            
-
-          
 
           
           ?> 
-        
-
-
-        
-
 
         
         </div>
@@ -1189,7 +1337,7 @@
           
           $strDate =  explode(" ", date("year"), 8);
            
-          $monthNummber = date("m");
+          $monthNummber = $_COOKIE["ViewMonthData"];
           $d=cal_days_in_month(CAL_GREGORIAN,$monthNummber,$strDate[3]);
 
 
@@ -1343,35 +1491,13 @@
 
                  
                     }
-
-               
-
-
-
-                  
-
-
-                 
- 
-            
+  
                 }
 
 
-        
-            
-
-          
-
           
           ?> 
-        
 
-
-        
-
-
-        
-        
 
         </div>
         <div class="header" id="Sunday">Sonntag
@@ -1381,7 +1507,7 @@
           
           $strDate =  explode(" ", date("year"), 8);
            
-          $monthNummber = date("m");
+          $monthNummber = $_COOKIE["ViewMonthData"];
           $d=cal_days_in_month(CAL_GREGORIAN,$monthNummber,$strDate[3]);
 
 
@@ -1523,35 +1649,12 @@
                  
                     }
 
-               
-
-
-
-                  
-
-
-                 
- 
             
                 }
-
-
-        
-            
-
-          
 
           
           ?> 
         
-
-
-        
-
-
-        
-        
-
         
         </div>
        
@@ -1564,15 +1667,24 @@
 
     </div>
 
-    <button class="right" onclick="GetPreviousMonth()">Zurück</button>
-    <button class="left" onclick="GetNextMonth()">Nächste</button>
+
+<form method="post">
+   
+    <input type="submit" name="LastMonth" class="right" value="Zurück" > 
+    <input type="submit" name="AddToAppointments" class="TopShowRight" value="asdass" > 
+    <input type="submit" name="NextMonth" class="left" value="Nächste" > 
+</form>
+
+
+ 
+ 
     <button id="BackTodateButton" class="TopHidden" onclick="getCurentDate()">Zurück zu Heute</button>
 
 
 
 </section>
 
-<scrip src = "Scripts/CDate.php"></script>
+<scrip src = "CollorChanger.php"></script>
     
 </body>
 
